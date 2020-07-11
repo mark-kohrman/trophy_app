c1ee013893d1bb6198f164839a8c4882bc4e59d0
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-10 23:13:48 -0500

    adding the update and destroy action

--- app/controllers/api/trophies_controller.rb
+++ app/controllers/api/trophies_controller.rb
@@ -20,4 +20,22 @@ class Api::TrophiesController < ApplicationController
     @trophy.save
     render 'show.json.jb'
   end
+  
+  def update
+    @trophy = Trophy.find_by(id: params[:id]  )
+    @trophy.update(
+      metal: params[:metal] || @trophy.metal,
+      color: params[:color] || @trophy.color,
+      wood_type: params[:wood_type] || @trophy.wood_type,
+      price: params[:price] || @trophy.price
+    )
+    @trophy.save
+    render 'show.json.jb'
+  end
+
+  def destroy
+    @trophy = Trophy.find_by(id: params[:id])
+    @trophy.destroy
+    render json: {message: "You're trophy was destroyed cuz you lost"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     get "/trophies" => "trophies#index"
     get "/trophies/:id" => "trophies#show"
     post "/trophies" => "trophies#create"
+    patch "/trophies/:id" => "trophies#update"
+    delete "/trophies/:id" => "trophies#destroy "
   end
 end
